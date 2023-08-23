cb2516a949ee1594b6abe9bfbd53de20bdeca28b
Suyeon-B <tndus4243@naver.com>
2023-08-24 01:36:35 +0900

    chore: 프로젝트 세팅

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      className="bg-cover bg-center h-screen relative"
+      style={{ backgroundImage: `url('/assets/background/evening-city.jpg')` }}
+    >
+      <h1 className="text-sky-50 text-5xl">푸푸런 💩</h1>
+    </div>
+  );
+}
+
+export default App;
